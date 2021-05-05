a386b764096fb07fc60315745bf0a660b64ed326
Ocelots-etc <catherinedgomez@gmail.com>
2021-05-04 23:29:46 -0500

    update dreams controller with create and routes

--- app/controllers/api/v1/dreams_controller.rb
+++ app/controllers/api/v1/dreams_controller.rb
@@ -4,5 +4,21 @@ class Api::V1::DreamsController < ApplicationController
     dreams = Dream.all
     render json: dreams
   end
+ 
+  def create
+    dream = Dream.new(dream_params)
+    if dream.save
+      render json: dream, status: :accepted
+    else
+      render json: {errors: dream.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:dream_datetime, :journal, :image_url, :category_id)
+  end
 
 end
